30a5c081151037e4354a092c70d36bf63322252f
Enhancer <xu.jiahao@darcytech.com>
2022-05-23 12:31:34 +0800

    Fix pagination, add auto-retry, skip time zoned pages

--- notion_timezone_setter.py
+++ notion_timezone_setter.py
@@ -1,3 +1,5 @@
+from json import JSONDecodeError
+
 import requests
 import properties
 import os
@@ -17,18 +19,44 @@ else:
 header = {"Authorization": NOTION_TOKEN, "Notion-Version": "2022-02-22", "Content-Type": "application/json"}
 
 
-def query():
-    return requests.post(base_url + "databases/" + DATABASE_ID + "/query", headers=header, data='{}')
+def query(start_cursor):
+    if start_cursor == "":
+        body = '{}'
+    else:
+        body = '{"start_cursor": "' + str(start_cursor) + '"}'
+    return requests.post(base_url + "databases/" + DATABASE_ID + "/query", headers=header, data=body).json()
 
 
 def update_page_date(page):
     page_id = page["id"]
     start = page["properties"][DATE_COLUMN_NAME]["date"]["start"]
+    current_time_zone = page["properties"][DATE_COLUMN_NAME]["date"]["time_zone"]
+
+    if current_time_zone == "Asia/Shanghai":
+        return
+
     start = str(start).split('.')[0]
     body = '{"properties": {"' + DATE_COLUMN_NAME + '": {"date": {"start": "' + start + '","time_zone":"Asia/Shanghai"}}}}'
-    return requests.patch(base_url + "pages/" + page_id, headers=header, data=body.encode('utf-8'))
+    return __retry_patch(base_url + "pages/" + page_id, header, body.encode('utf-8'))
 
 
-pages = query().json()["results"]
+def __retry_patch(url, header, body):
+    try:
+        resp = requests.patch(url, headers=header, data=body).json()
+    except JSONDecodeError:
+        resp = __retry_patch(url, header, body)
+
+    return resp
+
+
+pages = []
+has_more = True
+start_cursor = ""
+while has_more:
+    response = query(start_cursor)
+    has_more = response["has_more"]
+    start_cursor = response["next_cursor"]
+    pages.extend(response["results"])
 for p in pages:
-    print(update_page_date(p).json())
+    response = update_page_date(p)
+    print(update_page_date(p))
